d9762f917923d44f0812de3935bb7db54ff57d36
TurBoss <j.l.toledano.l@gmail.com>
2018-12-17 00:39:49 +0100

    Get DynATC from old branch

---
+++ qtpyvcp/widgets/input_widgets/atc_widget/atc.py
@@ -0,0 +1,97 @@
+import os
+
+# Workarround for nvidia propietary drivers
+
+import ctypes
+import ctypes.util
+
+ctypes.CDLL(ctypes.util.find_library("GL"), mode=ctypes.RTLD_GLOBAL)
+
+# end of Workarround
+
+from hal import component, HAL_BIT, HAL_IN
+
+from qtpy.QtCore import Signal, Slot, QUrl
+from qtpy.QtQuickWidgets import QQuickWidget
+
+from qtpyvcp.plugins import getPlugin
+
+STATUS = getPlugin('status')
+
+WIDGET_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+# def _initComp():
+#     halcomp = component("atc_widget")
+#     halcomp.newpin("fwd", HAL_BIT, HAL_IN)
+#     halcomp.newpin("rev", HAL_BIT, HAL_IN)
+#     halcomp.ready()
+#
+#     return halcomp
+
+
+class DynATC(QQuickWidget):
+
+    def __init__(self, parent=None):
+        super(DynATC, self).__init__(parent)
+
+        if parent is None:
+            return
+
+        self.engine().rootContext().setContextProperty("atc_spiner", self)
+        url = QUrl.fromLocalFile(os.path.join(WIDGET_PATH, "atc.qml"))
+        self.setSource(url)
+
+        # try:
+        #     self.halcomp = _initComp()
+        # except Exception as e:
+        #     self.halcomp = None
+
+        self.fwd_pin = 0
+        self.rev_pin = 0
+
+        self.prev_fwd_pin = 0
+        self.prev_rev_pin = 0
+
+        self.atc_position = 0
+
+        # STATUS.tool_in_spindle.connect(self.on_tool_in_spindle)
+        # STATUS.pocket_prepped.connect(self.on_pocket_prepped)
+
+    def on_pocket_prepped(self, pocket_num):
+        print "Pocket Prepared: ", pocket_num
+
+    def on_tool_in_spindle(self, tool_num):
+        print "Tool in Spindle: ", tool_num
+
+    rotateFwdSig = Signal(int, arguments=['rotate_forward'])
+    @Slot()
+    def rotate_forward(self):
+        self.rotateFwdSig.emit(self.atc_position)
+        self.atc_position += 1
+
+    rotateRevSig = Signal(int, arguments=['rotate_reverse'])
+    @Slot()
+    def rotate_reverse(self):
+        self.rotateRevSig.emit(self.atc_position)
+        self.atc_position -= 1
+
+    pinSig = Signal(arguments=['get_pins'])
+    @Slot()
+    def get_pins(self):
+        pass
+
+        # if self.halcomp is None:
+        #    return
+        #
+        # self.fwd_pin = self.halcomp["fwd"]
+        # if self.fwd_pin != self.prev_fwd_pin:
+        #     self.prev_fwd_pin = self.fwd_pin
+        #     self.rotateFwdSig.emit(self.atc_position)
+        #     self.atc_position += 1
+        #
+        # self.rev_pin = self.halcomp["rev"]
+        # if self.rev_pin != self.prev_rev_pin:
+        #     self.prev_rev_pin = self.rev_pin
+        #     self.rotateRevSig.emit(self.atc_position)
+        #     self.atc_position += 1
--- qtpyvcp/widgets/input_widgets/designer_plugins.py
+++ qtpyvcp/widgets/input_widgets/designer_plugins.py
@@ -3,6 +3,8 @@
 from qtpyvcp.widgets.qtdesigner import _DesignerPlugin
 
 from mdientry_widget import MDIEntry
+
+
 class MDIEntryPlugin(_DesignerPlugin):
     def pluginClass(self):
         return MDIEntry
@@ -10,42 +12,74 @@ class MDIEntryPlugin(_DesignerPlugin):
     def toolTip(self):
         return "MDI command entry"
 
+
 from gcode_editor import GcodeEditor
+
+
 class GcodeEditorPlugin(_DesignerPlugin):
     def pluginClass(self):
         return GcodeEditor
 
+
 from recent_file_combobox import RecentFileComboBox
+
+
 class RecentFileComboBoxPlugin(_DesignerPlugin):
     def pluginClass(self):
         return RecentFileComboBox
 
+
 from tool_table import ToolTable
+
+
 class ToolTablePlugin(_DesignerPlugin):
     def pluginClass(self):
         return ToolTable
 
+
 from jog_increment import JogIncrementWidget
+
+
 class JogIncrementPlugin(_DesignerPlugin):
     def pluginClass(self):
         return JogIncrementWidget
 
+
 from file_system import FileSystemTable
+
+
 class FileSystemPlugin(_DesignerPlugin):
     def pluginClass(self):
         return FileSystemTable
 
+
 from file_system import RemovableDeviceComboBox
+
+
 class RemovableDeviceComboBoxPlugin(_DesignerPlugin):
     def pluginClass(self):
         return RemovableDeviceComboBox
 
+
 from action_slider import ActionSlider
+
+
 class ActionSliderPlugin(_DesignerPlugin):
     def pluginClass(self):
         return ActionSlider
 
+
 from action_combobox import ActionComboBox
+
+
 class ActionComboBoxPlugin(_DesignerPlugin):
     def pluginClass(self):
         return ActionComboBox
+
+
+from atc_widget.atc import DynATC
+
+
+class DynATCPlugin(_DesignerPlugin):
+    def pluginClass(self):
+        return DynATC
